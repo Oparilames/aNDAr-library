abcde024358ca9d3d50f2c7ac7d7e673e3977408
Oparilames <49676460+Oparilames@users.noreply.github.com>
2021-11-23 20:41:28 +0000

    Delete variadicParameterPacks.cpp

--- variadicParameterPacks.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright (C) 2021 Oparilames <oparilames@arcor.de>
-// This file is subject to the GNU General Public License v3.0.
-// Exact license terms can be found in the LICENSE file
-// found in the top-level directory of this distribution.
